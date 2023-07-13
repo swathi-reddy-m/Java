d03ddbac3e3446077f8f1a00f511e1de70cdb508
Mekala Swathi Reddy <89543655+swathi-reddy-m@users.noreply.github.com>
2023-07-13 13:59:10 +0530

    Create input_output.java

---
+++ Programs/input_output.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class A {
+  public static void main(String args[]) {
+    Scanner sc=new Scanner(system.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
