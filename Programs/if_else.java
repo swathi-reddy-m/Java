a3ca7e7dd744694e17a6650454d619ae4c5493cd
Mekala Swathi Reddy <89543655+swathi-reddy-m@users.noreply.github.com>
2023-07-14 22:46:13 +0530

    Create if_else.java

---
+++ Programs/if_else.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class A{
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int N = scanner.nextInt();
+      if(N%2!=0) {
+        System.out.println("Weird");
+      }
+      else {
+        if(N>=2 && N<=5) {
+            System.out.println("Not Weird");
+        }
+        else if(N>=6 && N<=20) {
+            System.out.println("Weird");
+        }
+        else if(N>20) {
+            System.out.println("Not Weird");
+        }
+      }
+    }
+}
